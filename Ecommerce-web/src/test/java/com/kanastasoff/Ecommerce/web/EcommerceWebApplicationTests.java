0946f1cd5674df28c3e0e3875bb54cd8ca80b856
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-03 15:52:41 +0200

    new eccomerse started app

---
+++ Ecommerce-web/src/test/java/com/kanastasoff/Ecommerce/web/EcommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanastasoff.Ecommerce.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- ecommerce/src/main/java/com/kirilanastasoff/ecommerce/Dev.java
+++ ecommerce/src/main/java/com/kirilanastasoff/ecommerce/Dev.java
@@ -6,19 +6,34 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public Dev(int age) {
+        this.age = age;
+    }
+
+    private int age;
     @Autowired
     @Qualifier("laptop")
     private Laptop laptop = new Laptop();
 
-//    public Dev(Laptop laptop) {
-//        this.laptop = laptop;
-//    }
-//
-//    @Autowired
-//    public void setLaptop(Laptop laptop) {
-//        this.laptop = laptop;
-//    }
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Autowired
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+
 
     public void build(){
         laptop.compile();
